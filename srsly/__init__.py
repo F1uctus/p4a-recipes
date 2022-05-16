be0fba29884d10f8189f973eda69b07792e213a1
Ilya Nikitin <ilya.i.nikitin@proton.me>
2022-05-17 01:31:58 +0300

    Update srsly to 2.4.3

--- srsly/__init__.py
+++ srsly/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 
 class SrslyRecipe(CompiledComponentsPythonRecipe):
-    version = "2.4.2"
+    version = "2.4.3"
     url = "https://pypi.python.org/packages/source/s/srsly/srsly-{version}.tar.gz"
     site_packages_name = "srsly"
     depends = ["setuptools", "cython", "catalogue"]
