2c87ea46691c58f1513c6a5a676afe48c3cce1cf
Ilya Nikitin <ilya.i.nikitin@proton.me>
2022-05-17 03:17:33 +0300

    Re-apply fix of BLIS_COMPILER env var setup

--- blis/__init__.py
+++ blis/__init__.py
@@ -15,12 +15,23 @@ class BlisRecipe(CompiledComponentsPythonRecipe):
     def get_recipe_env(self, arch=None, with_flags_in_cc=False):
         env = super().get_recipe_env(arch, with_flags_in_cc)
 
+        # see https://github.com/flame/blis/blob/master/config_registry
         if isinstance(arch, ArchAarch_64):
             env["BLIS_ARCH"] = "arm64"
         elif isinstance(arch, Archx86_64):
             env["BLIS_ARCH"] = "x86_64"
         else:
             env["BLIS_ARCH"] = "generic"
+
+        cli = env["CC"].split()[0]
+        ccache_bin = cli if "ccache" in cli else ""
+
+        env["BLIS_COMPILER"] = " ".join(
+            [
+                ccache_bin,
+                arch.get_clang_exe(with_target=True),
+            ]
+        )
 
         return env
 
