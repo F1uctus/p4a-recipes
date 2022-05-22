d343779f15a6470db9dbda59cb88fedc84b66323
Ilya Nikitin <ilya.i.nikitin@proton.me>
2022-05-23 00:16:30 +0300

    Fix `CppCompiledComponentsPythonRecipe` compilation failure for `thinc` and `spacy`

--- spacy/__init__.py
+++ spacy/__init__.py
@@ -1,19 +1,20 @@
-from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 
-class SpacyRecipe(CppCompiledComponentsPythonRecipe):
+class SpacyRecipe(CompiledComponentsPythonRecipe):
     version = "3.3.0"
     url = "https://pypi.python.org/packages/source/s/spacy/spacy-{version}.tar.gz"
     site_packages_name = "spacy"
     depends = [
+        "setuptools",
+        "cython",
+        # Explosion-provided dependencies
         "spacy-legacy",
         "spacy-loggers",
         "cymem",
-        "click",
         "preshed",
         "thinc",
         "blis",
-        "markupsafe",
         "ml_datasets",
         "murmurhash",
         "wasabi",
@@ -21,23 +22,38 @@ class SpacyRecipe(CppCompiledComponentsPythonRecipe):
         "catalogue",
         "typer",
         "pathy",
+        # Third party dependencies
         "numpy",
         "requests",
-        "urllib3",
         "tqdm",
         "pydantic",
+        "jinja2",
+        "langcodes",
+        # Official Python utilities
+        "packaging",
         # Required for Russian language
         "pymorphy2",
         "pymorphy2_dicts_ru",
         "DAWG-Python",
         "appdirs",
         "pyparsing",
-        "jinja2",
-        "langcodes",
-        "setuptools",
-        "packaging",
-        "typing_extensions",
     ]
+    call_hostpython_via_targetpython = False
+
+    def get_recipe_env(self, arch=None, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        env["CXXFLAGS"] = env["CFLAGS"] + " -frtti -fexceptions"
+
+        if with_flags_in_cc:
+            env["CXX"] += " -frtti -fexceptions"
+
+        env["LDFLAGS"] += " -L{}".format(self.get_stl_lib_dir(arch))
+        env["LIBS"] = env.get("LIBS", "") + " -l{}".format(self.stl_lib_name)
+        return env
+
+    def postbuild_arch(self, arch):
+        super().postbuild_arch(arch)
+        self.install_stl_lib(arch)
 
 
 recipe = SpacyRecipe()
--- thinc/__init__.py
+++ thinc/__init__.py
@@ -1,10 +1,10 @@
-from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 
-class ThincRecipe(CppCompiledComponentsPythonRecipe):
+class ThincRecipe(CompiledComponentsPythonRecipe):
     version = "master"
     url = (
-        "https://github.com/explosion/thinc/archive/master.tar.gz"
+        "https://github.com/explosion/thinc/archive/90631684f8e4448fb5894cc8ab748a68939f2654.tar.gz"
         if version == "master"
         else "https://pypi.python.org/packages/source/t/thinc/thinc-{version}.tar.gz"
     )
@@ -12,7 +12,7 @@ class ThincRecipe(CppCompiledComponentsPythonRecipe):
     depends = [
         "setuptools",
         "cython",
-        # from requirements
+        # Explosion-provided dependencies
         "murmurhash",
         "cymem",
         "preshed",
@@ -21,10 +21,27 @@ class ThincRecipe(CppCompiledComponentsPythonRecipe):
         "wasabi",
         "catalogue",
         "ml_datasets",
+        # Third-party dependencies
         "pydantic",
         "numpy",
     ]
+    call_hostpython_via_targetpython = False
     install_in_hostpython = True
+
+    def get_recipe_env(self, arch=None, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        env["CXXFLAGS"] = env["CFLAGS"] + " -frtti -fexceptions"
+
+        if with_flags_in_cc:
+            env["CXX"] += " -frtti -fexceptions"
+
+        env["LDFLAGS"] += " -L{}".format(self.get_stl_lib_dir(arch))
+        env["LIBS"] = env.get("LIBS", "") + " -l{}".format(self.stl_lib_name)
+        return env
+
+    def postbuild_arch(self, arch):
+        super().postbuild_arch(arch)
+        self.install_stl_lib(arch)
 
 
 recipe = ThincRecipe()
