1455b6b67d0a991f1093bf795451d934f688802b
Ilya Nikitin <ilya.i.nikitin@proton.me>
2022-05-22 16:06:16 +0300

    `SrslyRecipe`, `PreshedRecipe`: Reformat

--- preshed/__init__.py
+++ preshed/__init__.py
@@ -5,7 +5,12 @@ class PreshedRecipe(CompiledComponentsPythonRecipe):
     version = "3.0.6"
     url = "https://pypi.python.org/packages/source/p/preshed/preshed-{version}.tar.gz"
     site_packages_name = "preshed"
-    depends = ["setuptools", "cython", "cymem", "murmurhash"]
+    depends = [
+        "setuptools",
+        "cython",
+        "cymem",
+        "murmurhash",
+    ]
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
--- srsly/__init__.py
+++ srsly/__init__.py
@@ -5,7 +5,11 @@ class SrslyRecipe(CompiledComponentsPythonRecipe):
     version = "2.4.3"
     url = "https://pypi.python.org/packages/source/s/srsly/srsly-{version}.tar.gz"
     site_packages_name = "srsly"
-    depends = ["setuptools", "cython", "catalogue"]
+    depends = [
+        "setuptools",
+        "cython",
+        "catalogue",
+    ]
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
