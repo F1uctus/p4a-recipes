fe7e072a0c40128057cb6097319e45fcb62e4223
Ilya Nikitin <ilya.i.nikitin@proton.me>
2022-05-22 16:06:38 +0300

    Patch p4a-bundled `CythonRecipe` to use ccache

---
+++ cython/__init__.py
@@ -0,0 +1,19 @@
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+
+
+class CythonRecipe(CompiledComponentsPythonRecipe):
+
+    version = "0.29.30"
+    url = "https://github.com/cython/cython/archive/{version}.tar.gz"
+    site_packages_name = "cython"
+    depends = ["setuptools"]
+    call_hostpython_via_targetpython = False
+    install_in_hostpython = True
+
+    def get_hostrecipe_env(self, arch):
+        env = super().get_hostrecipe_env(arch)
+        env["CC"] = "/usr/bin/ccache gcc"
+        return env
+
+
+recipe = CythonRecipe()
