fee96ebf00f1e6bd28e911e84f2bb3dda465a689
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-01-28 16:01:12 +0300

    preshed: try fix cymem+murmurhash deps

--- preshed/__init__.py
+++ preshed/__init__.py
@@ -1,4 +1,4 @@
-from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe, Recipe
 
 
 class PreshedRecipe(CompiledComponentsPythonRecipe):
@@ -13,6 +13,23 @@ class PreshedRecipe(CompiledComponentsPythonRecipe):
     ]
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
+
+    def get_recipe_env(self, arch=None, with_flags_in_cc=False):
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        if arch is None:
+            return env
+
+        # Make sure Cython can find .pxd files from dependencies when building
+        # via hostpython (e.g. `from cymem.cymem cimport Pool`).
+        dep_build_dirs = [
+            Recipe.get_recipe("cymem", self.ctx).get_build_dir(arch.arch),
+            Recipe.get_recipe("murmurhash", self.ctx).get_build_dir(arch.arch),
+        ]
+        existing = env.get("CYTHON_INCLUDE_PATH")
+        env["CYTHON_INCLUDE_PATH"] = ":".join(
+            ([existing] if existing else []) + dep_build_dirs
+        )
+        return env
 
 
 recipe = PreshedRecipe()
