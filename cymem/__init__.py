eeb18e339e832e502ec305f049384063f679c518
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-01-29 00:33:23 +0300

    code reformat

--- cymem/__init__.py
+++ cymem/__init__.py
@@ -5,7 +5,10 @@ class CymemRecipe(CompiledComponentsPythonRecipe):
     version = "2.0.13"
     url = "https://files.pythonhosted.org/packages/source/c/cymem/cymem-{version}.tar.gz"
     site_packages_name = "cymem"
-    depends = ["setuptools", "cython"]
+    depends = [
+        "setuptools",
+        "cython",
+    ]
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
--- murmurhash/__init__.py
+++ murmurhash/__init__.py
@@ -5,7 +5,10 @@ class MurmurhashRecipe(CompiledComponentsPythonRecipe):
     version = "1.0.15"
     url = "https://files.pythonhosted.org/packages/source/m/murmurhash/murmurhash-{version}.tar.gz"
     site_packages_name = "murmurhash"
-    depends = ["setuptools", "cython"]
+    depends = [
+        "setuptools",
+        "cython",
+    ]
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
