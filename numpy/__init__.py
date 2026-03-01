59a47fb4bbc1b3ce0b47b806dedf5e3c6e976be6
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-03-01 12:15:30 +0300

    Fix local CI via nektos/act. Fix ENV PATH syntax in build-env. Silence "stty: 'standard input': Inappropriate ioctl for device".

--- numpy/__init__.py
+++ numpy/__init__.py
@@ -1,6 +1,9 @@
 from pythonforandroid.recipe import Recipe, MesonRecipe
-from os.path import join
+from pythonforandroid.util import current_directory, ensure_dir
+from pythonforandroid.logger import shprint
+from os.path import join, isdir
 import shutil
+import sh
 
 NUMPY_NDK_MESSAGE = (
     "In order to build numpy, you must set minimum ndk api (minapi) to `24`.\n"
@@ -9,7 +12,7 @@ NUMPY_NDK_MESSAGE = (
 
 class NumpyRecipe(MesonRecipe):
     version = "v2.3.0"
-    url = "git+https://github.com/numpy/numpy"
+    url = "https://github.com/numpy/numpy"
     hostpython_prerequisites = [
         "Cython>=3.0.6",
         "numpy",
@@ -17,6 +20,18 @@ class NumpyRecipe(MesonRecipe):
     extra_build_args = ["-Csetup-args=-Dblas=none", "-Csetup-args=-Dlapack=none"]
     need_stl_shared = True
     min_ndk_api_support = 24
+
+    def download_file(self, url, target, cwd=None):
+        if cwd:
+            target = join(cwd, target)
+        if not isdir(target):
+            ensure_dir(target)
+            shprint(sh.git, 'clone', '--recursive', url, target)
+        with current_directory(target):
+            shprint(sh.git, 'fetch', '--tags', '--force', '--depth', '1')
+            shprint(sh.git, 'checkout', self.version)
+            shprint(sh.git, 'submodule', 'update', '--recursive', '--init', '--depth', '1')
+        return target
 
     def get_recipe_meson_options(self, arch):
         options = super().get_recipe_meson_options(arch)
