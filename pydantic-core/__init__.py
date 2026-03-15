7b2aae20ae4b707123b82ca30e16d5234286c61b
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-03-16 01:10:24 +0300

    Fix pydantic-core architecture error (wrong URL and build dir jump)

--- pydantic-core/__init__.py
+++ pydantic-core/__init__.py
@@ -5,14 +5,10 @@ from os.path import join
 
 class PydanticcoreRecipe(RustCompiledComponentsRecipe):
     version = "2.42.0"
-    url = "https://github.com/pydantic/pydantic/archive/refs/tags/v2.13.0b2.zip"
+    url = "https://github.com/pydantic/pydantic-core/archive/refs/tags/v{version}.zip"
     site_packages_name = "pydantic_core"
-    depends = ["python3", "setuptools"]
-
-    def build_arch(self, arch):
-        build_dir = self.get_build_dir(arch.arch)
-        with current_directory(join(build_dir, "pydantic-core")):
-            super().build_arch(arch)
+    depends = ["python3", "setuptools", "hostpython3"]
+    hostpython_prerequisites = ["maturin>=1.5.0"]
 
 
 recipe = PydanticcoreRecipe()
