db147ba2ee50e66be4c7fb73ee2fa14d7ca6cd9a
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-03-15 13:24:29 +0300

    fix pydantic-core source location

--- pydantic-core/__init__.py
+++ pydantic-core/__init__.py
@@ -1,10 +1,18 @@
 from pythonforandroid.recipe import RustCompiledComponentsRecipe
+from pythonforandroid.toolchain import current_directory
+from os.path import join
 
 
 class PydanticcoreRecipe(RustCompiledComponentsRecipe):
     version = "2.42.0"
-    url = "https://github.com/pydantic/pydantic-core/archive/refs/tags/v{version}.tar.gz"
+    url = "https://github.com/pydantic/pydantic/archive/refs/tags/v2.13.0b2.zip"
     site_packages_name = "pydantic_core"
+    depends = ["python3", "setuptools"]
+
+    def build_arch(self, arch):
+        build_dir = self.get_build_dir(arch.arch)
+        with current_directory(join(build_dir, "pydantic-core")):
+            super().build_arch(arch)
 
 
 recipe = PydanticcoreRecipe()
