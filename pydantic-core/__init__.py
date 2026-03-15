7a6edca10ba608d904b3dd187debfbd0b8cdd5bf
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-03-16 01:12:38 +0300

    Use PyPI source for pydantic-core to avoid 404 on GitHub tags

--- pydantic-core/__init__.py
+++ pydantic-core/__init__.py
@@ -5,7 +5,7 @@ from os.path import join
 
 class PydanticcoreRecipe(RustCompiledComponentsRecipe):
     version = "2.42.0"
-    url = "https://github.com/pydantic/pydantic-core/archive/refs/tags/v{version}.zip"
+    url = "https://files.pythonhosted.org/packages/source/p/pydantic-core/pydantic_core-{version}.tar.gz"
     site_packages_name = "pydantic_core"
     depends = ["python3", "setuptools", "hostpython3"]
     hostpython_prerequisites = ["maturin>=1.5.0"]
